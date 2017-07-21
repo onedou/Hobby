bf4d90abdd649c7239ed9b68334db54de9e0926e
onedou <onedou@126.com>
2017-07-21 08:57:22 +0800

    update
    Signed-off-by: onedou <onedou@126.com>

---
+++ ruby/helloruby.rb
@@ -0,0 +1,33 @@
+print("Hello, Ruby.\n")
+
+puts "Hello", "RUBY!"
+
+p "100"
+p 100
+
+p "测试测试测试"
+
+p Math.sin(3.1415)
+p Math.sqrt(10000)
+
+area = 10;
+
+print "表面积=#{area}"
+
+=begin
+	
+rescue Exception => e
+	
+end
+
+=end
+
+i = 1
+while i <= 20
+	print i, "\n"
+	i = i + 1
+end
+
+100.times do
+	print "All work and no play makes Jack a dull boy.\n"
+end
